dbb64e9be5f4766df54f7ab0c1163deb54b3c9ef
Matthew Sumner <matt.m.sumner@gmail.com>
2015-03-20 13:33:28 -0400

    Refactor receipt_printer to extract some methods
    Why:
    
    * The print method is too complex.
    
    This PR:
    
    * Extracts methods to better describe what operations are being
    performed.

--- lib/receipt_printer.rb
+++ lib/receipt_printer.rb
@@ -13,23 +13,61 @@ class ReceiptPrinter
   end
 
   def print
-    subtotal = items.reduce(0) do |sum, item|
-      item_cost = COST[item]
-      output.puts "#{item}: #{sprintf('$%.2f', item_cost)}"
-
-      sum + item_cost.to_i
-    end
-
-    output.puts divider
-    output.puts "subtotal: #{sprintf('$%.2f', subtotal)}"
-    output.puts "tax: #{sprintf('$%.2f', subtotal * TAX)}"
-    output.puts divider
-    output.puts "total: #{sprintf('$%.2f', subtotal + (subtotal * TAX))}"
+    output_items
+    output_subtotals
+    output_total
   end
 
   private
 
   attr_reader :output, :items
+
+  def output_items
+    item_lines
+    divider_line
+  end
+
+  def output_subtotals
+    subtotal_line
+    tax_line
+    divider_line
+  end
+
+  def item_lines
+    items.each do |item|
+      output.puts "#{item}: #{sprintf('$%.2f', item_cost(item))}"
+    end
+  end
+
+  def subtotal
+    @_subtotal ||= items.reduce(0) do |sum, item|
+      sum + item_cost(item).to_i
+    end
+  end
+
+  def item_cost(item)
+    COST[item]
+  end
+
+  def divider_line
+    output.puts divider
+  end
+
+  def subtotal_line
+    output_with label: "subtotal", cost: subtotal
+  end
+
+  def tax_line
+    output_with label: "tax", cost: subtotal * TAX
+  end
+
+  def output_total
+    output_with label: "total", cost: subtotal + (subtotal * TAX)
+  end
+
+  def output_with(label:, cost:)
+    output.puts "#{label}: #{sprintf('$%.2f', cost)}"
+  end
 
   def divider
     '-' * 13
